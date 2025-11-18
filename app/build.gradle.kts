aa9bdd601dc45ccf4ff3b6fa21463e0f6442e750
Abd-Ur-Raheem <abdurraheem.raheem@mail.utoronto.ca>
2025-11-18 12:59:24 -0500

    not complete yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.firebase:firebase-database:20.3.0")
 }
--- app/src/main/java/com/example/smartair/MainActivity.java
+++ app/src/main/java/com/example/smartair/MainActivity.java
@@ -1,24 +1,26 @@
- package com.example.smartair;
+package com.example.smartair;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button loginBtn = findViewById(R.id.login_button_main);
+        Button registerBtn = findViewById(R.id.register_button_main);
+
+        loginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+        registerBtn.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
         });
     }
 }
